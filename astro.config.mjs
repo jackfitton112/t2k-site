e076575d1d9be8bdfe6259a155d9f4a37cbe4e24
Jack Fitton <93110524+jackfitton112@users.noreply.github.com>
2022-10-31 12:18:25 +0000

    Lots of changes, improved preformance

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,18 @@
 import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
+import robotsTxt from 'astro-robots-txt';
+import sitemap from "@astrojs/sitemap";
 
+// https://astro.build/config
+import compress from "astro-compress";
+
+// https://astro.build/config
 export default defineConfig({
   site: "https://t2k-group.co.uk",
-  integrations: [tailwind()],
+  integrations: [tailwind(), sitemap(), robotsTxt(), compress()],
   vite: {
     ssr: {
-      external: ["@11ty/eleventy-img", "svgo"],
-    },
-  },
+      external: ["@11ty/eleventy-img", "svgo"]
+    }
+  }
 });
